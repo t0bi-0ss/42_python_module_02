4c10a4e10573f7734b017c4e52180afbf3f12627
Tobias Sordo Ostolaza <tsordo-o@c1r2s4.42malaga.com>
2026-06-19 14:57:44 +0200

    Write required program

---
+++ ex2/ft_different_errors.py
@@ -0,0 +1,46 @@
+def garden_operations(operation_number: int):
+    match operation_number:
+        case 0:
+            return int("abc")
+        case 1:
+            return 1/0
+        case 2:
+            return open("/non/existent/file")
+        case 3:
+            return 1 + "abc"
+        case _:
+            print("Operation completed successfully")
+            return
+        
+
+def test_error_type() -> None:
+    operation_number = 0
+    while (operation_number < 4):
+        operation_number += 1
+        print("Testing operation ", operation_number, "...")
+        try:
+            garden_operations(operation_number)
+        except ValueError:
+            print(
+                "Caught ValueError: invalid literal",
+                "for int() with base 10: 'abc'"
+                )
+        except ZeroDivisionError:
+            print(
+                "Caught ZeroDivisionError: division by zero"
+            )
+        except FileNotFoundError:
+            print(
+                "Caught FileNotFoundError: [Errno 2]",
+                " No such file or directory: 'non/existent/file'"
+            )
+        except TypeError:
+            print(
+                "Caught TypeError: can only concatenate",
+                " str (not \"int\") to str"
+            )
+    print("\nAll error types tested successfully")
+
+
+if __name__ == "__main__":
+    test_error_type()
