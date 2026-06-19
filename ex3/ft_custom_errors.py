96267e094c0eb40d6d295c08a08aa061d31cb437
Tobias Sordo Ostolaza <tsordo-o@c1r2s4.42malaga.com>
2026-06-19 15:55:31 +0200

    Add __str__ method to parent class and all children classes. Change command after exception catching in exception_handler()

--- ex3/ft_custom_errors.py
+++ ex3/ft_custom_errors.py
@@ -5,12 +5,19 @@ class GardenError(Exception):
         self.message = message
         super().__init__(self.message)
 
+    def __str__(self):
+        return f"{self.message}"
+
 class PlantError(GardenError):
     """GardenError for problems with plants"""
 
     def __init__(self, message: str = "Unknown plant error") -> None:
         self.message = message
         super().__init__(self.message)
+
+    def __str__(self):
+        return f"{self.message}"
+
 
 class WaterError(GardenError):
     """GardenError for problems with watering"""
@@ -19,6 +26,9 @@ class WaterError(GardenError):
         self.message = message
         super().__init__(self.message)
 
+    def __str__(self):
+        return f"{self.message}"
+    
 def plant_error_tester(error_number: int, error_message: str) -> None:
     """Raises a different exception depending on error number"""
 
@@ -31,3 +41,22 @@ def plant_error_tester(error_number: int, error_message: str) -> None:
             raise WaterError(error_message)
         case __:
             return
+
+
+def exception_handler(error_number: int, error_message: str) -> None:
+    """Handles all excepetions raised by plant_error_tester()"""
+
+    try:
+        plant_error_tester(error_number, error_message)
+    except GardenError as msg:
+        print(msg)
+    except PlantError as msg:
+        print(msg)
+    except WaterError as msg:
+        print(msg)
+
+
+if __name__ == "__main__":
+    print("=== Custom Garden Errors Demo ===\n")
+    print("Testing PlantError...")
+    exception_handler(1, "Caught PlantError: The tomato plant is wilting")
