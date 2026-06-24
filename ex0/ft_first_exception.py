dce93c55c889203eba748a0ad97e5856ba597f38
Tobias Sordo Ostolaza <tsordo-o@c1r2s4.42malaga.com>
2026-06-24 19:19:47 +0200

    Copy file from personal repo

---
+++ ex0/ft_first_exception.py
@@ -0,0 +1,33 @@
+def input_temperature(temp_str: str) -> int:
+    """Takes string representation of temperature
+    and return its int value"""
+    print(f"Input data is '{temp_str}'")
+    try:
+        temperature = int(temp_str)
+    except ValueError:
+        raise
+    else:
+        return temperature
+
+
+def test_temperature(temp_str: str) -> None:
+    """Checks if input_temperature() was succesfull"""
+    try:
+        temperature = input_temperature(temp_str)
+    except ValueError:
+        print(
+            "Caught input_temperature error: ",
+            "invalid literal for int() with base 10: '",
+            temp_str,
+            "'",
+        )
+    else:
+        print(f"Temperature is now {temperature}°C")
+
+
+if __name__ == "__main__":
+    print("=== Garden Temperature ===\n")
+    test_temperature("25")
+    print()
+    test_temperature("abc")
+    print("\nAll tests completed - program didn't crash!")
